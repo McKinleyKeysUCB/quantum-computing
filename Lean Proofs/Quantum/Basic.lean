3fd23aa8335b259037a1b412b39c4ff45740f8ed
McKinleyKeysUCB <mckinleykeys@berkeley.edu>
2024-04-15 18:40:52 -0700

    Finish proof of `tens_mul_tens`

--- Lean Proofs/Quantum/Basic.lean
+++ Lean Proofs/Quantum/Basic.lean
@@ -302,6 +302,35 @@ lemma eq_of_div_eq_div_and_mod_eq_mod {a b : ℕ} {x y : Fin (a * b)} (hdiv : x.
     apply Fin.ext
     rw [← Nat.div_add_mod ↑x b, ← Nat.div_add_mod ↑y b, hdiv, hmod]
 
+lemma Finset.sum_Fin_mul {α : Type} [AddCommMonoid α] {a b : ℕ} (f : Fin a → Fin b → α) :
+  (∑ x : Fin (a * b), f (Fin.divNat x) (Fin.modNat x)) = (∑ x : Fin a, ∑ y : Fin b, f x y)
+  := by
+    rw [← Fintype.sum_prod_type']
+    let g (x : Fin (a * b)) : Fin a × Fin b := ⟨Fin.divNat x, Fin.modNat x⟩
+    let AB : Finset (Fin (a * b)) := univ
+    have : univ (α := Fin a × Fin b) = image g AB := by
+      unfold_let g AB
+      rw [ext_iff]
+      intro x
+      constructor
+      · intro
+        rw [mem_image]
+        use div_mod_inv x.1 x.2
+        constructor
+        · simp only [mem_univ]
+        rw [divNat_div_mod_inv, modNat_div_mod_inv]
+      · intro hx
+        rw [mem_image] at hx
+        rcases hx with ⟨y, ⟨_, hxy⟩⟩
+        rw [← hxy]
+        simp only [mem_univ]
+    simp only [this]
+    rw [sum_image]
+    intro x _ y _ h
+    simp [g] at h
+    rcases h with ⟨left, right⟩
+    exact eq_of_div_eq_div_and_mod_eq_mod left right
+
 -- theorem tens_ext {M : QMatrix (m*p) (n*q)} (h : ∀ (r : Fin m) (s : Fin n) (v : Fin p) (w : Fin q), A r s * B v w = M (div_mod_inv r v) (div_mod_inv s w)) :
 --   -- A ⨂ B = M
 --   M = A ⨂ B
@@ -311,18 +340,15 @@ lemma eq_of_div_eq_div_and_mod_eq_mod {a b : ℕ} {x y : Fin (a * b)} (hdiv : x.
 lemma tens_mul_tens {a₁ b₁ c₁ a₂ b₂ c₂ : ℕ} {A : QMatrix a₁ b₁} {B : QMatrix a₂ b₂} {C : QMatrix b₁ c₁} {D : QMatrix b₂ c₂} :
   (A ⨂ B) * (C ⨂ D) = (A * C) ⨂ (B * D)
   := by
-    -- apply tens_ext
-    -- intro r s v w
-    -- unfold tens
-    -- rw [Matrix.mul_apply, Matrix.mul_apply, Matrix.mul_apply]
-    -- dsimp
-    simp
+    unfold tens
     apply Matrix.ext
     intro i j
-    simp [Matrix.mul_apply]
-    rw [Finset.sum_mul_sum]
-    
-    sorry
+    simp only [Matrix.mul_apply, Matrix.of_apply, Finset.sum_mul_sum, ← mul_assoc]
+    let f (x : Fin b₁) (y : Fin b₂) := (A i.divNat x) * (B i.modNat y) * (C x j.divNat) * (D y j.modNat)
+    change ∑ x : Fin (b₁ * b₂), f x.divNat x.modNat = ∑ x : Fin b₁, ∑ y : Fin b₂, (A i.divNat x) * (C x j.divNat) * (B i.modNat y) * (D y j.modNat)
+    rw [Finset.sum_Fin_mul]
+    unfold_let f
+    simp only [mul_left_comm, mul_right_comm, mul_assoc]
 
 /-
  - Use this notation to help Lean understand that two matrix types are the same. For example, Lean doesn't automatically understand that `QMatrix (2^3) (2^3)` and `QMatrix 8 8` are the same type. This example produces an error:
@@ -754,32 +780,3 @@ lemma sqrt_two_div_two_sq :
   Real.sqrt 2 / 2 * (Real.sqrt 2 / 2) = 1 / 2
   := by
     simp only [Real.sqrt_div_self', one_div, ← mul_inv, Nat.ofNat_nonneg, Real.mul_self_sqrt]
-
-lemma Finset.sum_Fin_mul {α : Type} [AddCommMonoid α] {a b : ℕ} (f : Fin a → Fin b → α) :
-  (∑ x : Fin (a * b), f (Fin.divNat x) (Fin.modNat x)) = (∑ x : Fin a, ∑ y : Fin b, f x y)
-  := by
-    rw [← Fintype.sum_prod_type']
-    let g (x : Fin (a * b)) : Fin a × Fin b := ⟨Fin.divNat x, Fin.modNat x⟩
-    let AB : Finset (Fin (a * b)) := univ
-    have : univ (α := Fin a × Fin b) = image g AB := by
-      unfold_let g AB
-      rw [ext_iff]
-      intro x
-      constructor
-      · intro
-        rw [mem_image]
-        use div_mod_inv x.1 x.2
-        constructor
-        · simp only [mem_univ]
-        rw [divNat_div_mod_inv, modNat_div_mod_inv]
-      · intro hx
-        rw [mem_image] at hx
-        rcases hx with ⟨y, ⟨_, hxy⟩⟩
-        rw [← hxy]
-        simp only [mem_univ]
-    simp only [this]
-    rw [sum_image]
-    intro x _ y _ h
-    simp [g] at h
-    rcases h with ⟨left, right⟩
-    exact eq_of_div_eq_div_and_mod_eq_mod left right
