a0e513d5d84ebb331a5c44c2d2e5ecbbffa4211c
McKinleyKeysUCB <mckinleykeys@berkeley.edu>
2024-04-16 11:44:38 -0700

    Update `Quantum.lean`

--- Lean Proofs/Quantum.lean
+++ Lean Proofs/Quantum.lean
@@ -1,3 +1,6 @@
+
 -- This module serves as the root of the `Quantum` library.
 -- Import modules here that should be built as part of the library.
-import «Quantum».Basic
+import «Quantum».Measurement
+import «Quantum».NoCloning
+import «Quantum».Teleportation
